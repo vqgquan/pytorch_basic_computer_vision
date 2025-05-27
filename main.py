c3a4f59cdd79e7d784d09f8ac72caa903896d6d4
vqgquan <quanvqgwork@gmail.com>
2025-05-27 19:07:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from typing import Annotated
-
+import torch
+import torchvision.transforms as transforms
 from fastapi import FastAPI, File, UploadFile
 
-app = FastAPI()
 
+
+model = torch.load("model.pth")
+model.eval()
+
+transform = transforms.Compose([
+    transforms.Resize((28, 28)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+
+app = FastAPI()
 
 @app.post("/files/")
 async def create_file(file: Annotated[bytes, File()]):
